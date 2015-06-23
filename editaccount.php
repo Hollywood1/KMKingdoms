126363287445629cb93734f9cb427e02d8e1f54a
NillBye <masdeoh@gmail.com>
2015-06-23 01:24:39 -0400

    Basic framework to litterally change anything about your account. OH BOY

--- editaccount.php
+++ editaccount.php
@@ -3,16 +3,22 @@ session_start();
 include("header.php");
 if(!isset($_SESSION['uid'])){
     echo "You must be logged in to view this page!";
-}else{
+}
+	if(isset($_POST['editaccount'])){
+	$house = $_POST['house'];
+	$upd1 = mysql_query("UPDATE `user` SET `house`='$house' WHERE `id`='$uid'") or die(mysql_error()); echo "You have updated your information"; }
+	else{
 	?>
-<b>Upload a flag</b><br /> *this doesn't work yet so*
-
-<form method="POST">
+<b>Upload a flag</b><br />
+<br />
+<b>House Information</b><br /><br />
+<form action="editaccount.php" method="POST">
 House: <input type="text" name="house"/><br /><br />
 Lord: <input type="text" name="currentlord"/><br /><br />
 <input type="submit" name="editaccount" value="Submit Changes"/>
 </form>
 <?php
 }
+
 include("footer.php");
 ?>
